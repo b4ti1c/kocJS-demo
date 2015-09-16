aa1618903e9a708f3c59e065efb670043f354fed
Hazar <hazar.ilhan@markafoni.com>
2015-09-16 14:54:57 +0300

    Initialize VM and VF in application.js

--- src/Application.js
+++ src/Application.js
@@ -2,6 +2,8 @@ goog.provide('mf.Application');
 goog.require('mf.helpers.GccKnockoutHelper');
 goog.require('mf.managers.ComponentRegistrationManager');
 goog.require('mf.ui.MainViewModel');
+goog.require('mf.managers.ViewManager');
+goog.require('mf.factories.ViewFactory');
 
 
 
@@ -11,7 +13,10 @@ goog.require('mf.ui.MainViewModel');
  * @constructor
  */
 mf.Application = function() {
-	mf.managers.ComponentRegistrationManager.getInstance();	 
 	ko.applyBindings(new mf.ui.MainViewModel());
+	mf.vf = mf.factories.ViewFactory.getInstance();
+	mf.vm = mf.managers.ViewManager.getInstance();
+	
+	mf.crm = mf.managers.ComponentRegistrationManager.getInstance();	 
 };
 goog.addSingletonGetter(mf.Application);
